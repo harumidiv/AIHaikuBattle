23889035fb372173aec63606a3e2df3ff9555770
harumidiv <h_sagawa@yumemi.co.jp>
2025-10-13 15:05:46 +0900

    プロジェクトを追加

---
+++ AIHaikuBattle/AIHaikuBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIHaikuBattleApp.swift
+//  AIHaikuBattle
+//
+//  Created by 佐川 晴海 on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct AIHaikuBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIHaikuBattle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIHaikuBattle
+//
+//  Created by 佐川 晴海 on 2025/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
